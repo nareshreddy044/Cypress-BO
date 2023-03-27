ee3ce8a47cffa1a96b02395190df44eb1dd152df
Naresh <nareshreddy044@gmail.com>
2023-03-27 13:17:42 +0530

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    
     setupNodeEvents(on, config) {
       //experimentalSessionAndOrigin: true
       // implement node event listeners here
     },
   },
 });
-module.exports = {
-  projectId: "hkzhdd"
-  // The rest of the Cypress config options go here...
-}
+// module.exports = {
+//   projectId: "hkzhdd"
+//   // The rest of the Cypress config options go here...
+// }
