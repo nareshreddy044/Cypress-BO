4b63042f4b61eed7a6ec985c3419e88b05ac97a6
Naresh <nareshreddy044@gmail.com>
2023-03-30 00:11:23 +0530

    test11

--- cypress/Pages/Transactions/Adjustment.js
+++ cypress/Pages/Transactions/Adjustment.js
@@ -1,120 +1,148 @@
 // const loc = require('cypress/Locators/Adjustment.json');
 // console.log(loc)
+import {constants} from '../Transactions/const.js';
+import { testdata } from "../../fixtures/testdata.js";
+import {Adjustloc} from "../Locators/Adjustment.js"
+
 require('@cypress/xpath');
 class adjustment
 {
 
     transactionprocessingbutton()
     {
-        cy.xpath("//a[contains(.,'Transaction Processing')]").click()
+        cy.xpath(Adjustloc.Txn_processing).click()
         
     }
     adjustmentselect()
     {
-        cy.xpath("//a[contains(text(),'Adjustment')]").click()
-        
+        cy.xpath(Adjustloc.Txn_selectadj).click()
     }
     addadjustment()
     {
-        cy.get('.justify-content-right > :nth-child(2)').click()
-       
-    }
-       
+        cy.get(Adjustloc.add_adjust).click()
+    }  
     adjustmentclienttype()
     {
-        cy.xpath("(//div[@class='below']//div)[1]").click()
+        cy.xpath(Adjustloc.client_type).click()
     }
     adjustmentbusinessclient()
     {
-        cy.xpath("//span[text()='Business']").click()
+        cy.xpath(Adjustloc.business_client).click()
     }
     adjustmentclient()
     {
-        cy.get('#walletAccountName').type("TVS infra llc")
+        cy.get(Adjustloc.client_name).type(testdata.clientname)
     }
     adjustmentclientselect()
     {
-        cy.get('.list-option').click()
+        cy.get(Adjustloc.client_select).click()
     }
     adjustmentaccountnumber()
     {
-        cy.xpath("(//div[@class='below'])[2]").click()
+        cy.xpath(Adjustloc.Account_numer).click()
     }
     adjustmentaccountnumberselect()
     {
-        cy.xpath("//span[text()='7110045189']").click()
+        cy.xpath(Adjustloc.Account_number_select).click()
     }
     adjustmentamount()
     {
-        cy.get("#amount").type("12")
+        cy.get(Adjustloc.Txn_amount).type(testdata.TxnAmount)
     }
     adjustmenttype()
     {
-        cy.xpath("//div[text()=' Adjustment Type ']").click()
+        cy.xpath(Adjustloc.Adj_type).click()
     }
     adjustmenttypeselect()
     {
-        cy.xpath("//span[text()='Manual Credit Adjustment']").click()
+        cy.xpath(Adjustloc.Adj_type_select).click()
     }
     adjustmenttransactionalremarks()
     {
-        cy.get(':nth-child(2) > :nth-child(3) > div.col-sm-12 > .dama-select-box-forminput').click()
+        cy.get(Adjustloc.Txn_Remarks).click()
     }
     adjustmenttransactionalremarksselect()
     {
-        cy.get(':nth-child(1) > .deselect-option').click()
+        cy.get(Adjustloc.Txn_Remarks_select).click()
     }
     adjustmentremarks()
     {
-        cy.get('#remarks1').type("remarks")
+        cy.get(Adjustloc.Remarks).type(testdata.Remarks)
     }
     adjustmentinternalremarks()
     {
-        cy.get('#internalRemarks').type("internalremarks")
+        cy.get(Adjustloc.Internal_remarks).type(testdata.InternalRemarks)
     }
     adjustmentsubmit()
     {
-        cy.get('[type="submit"]').click()
+        cy.get(Adjustloc.Submit).click()
     }
     adjustmentiframe()
     {
-        cy.xpath("//button[text()='okay']").click()
+        cy.xpath(Adjustloc.Ifram_select).click()
+    }
+    adjustmenttransactionid()
+    {
+        cy.wait(4000);
+        const getText = () => {
+            return cy.get(Adjustloc.Txn_id_select)
+              .then((adjustmenttransactionid) => {
+                 return adjustmenttransactionid.text(); 
+              });
+          }
+          getText().then((adjustmenttransactionid => {
+            constants.adjtransId = adjustmenttransactionid;
+           // cy.setCookie('transId', transactionid);
+            cy.log(constants.adjtransId);
+          }))
+          
     }
     adjustmentsearch()
     {
-        cy.get('.justify-content-left > .btn').click()
+        cy.get(Adjustloc.search).click()
     }
-    adjustmentsearchstatus()
+    adjustmenttransactionidtype()
     {
-        cy.get(':nth-child(2) > :nth-child(2) > .form-group > .col-sm-12 > .dama-select-box-forminput > .below > .single').click()
+        //var trxid = cashdeposittransactionid(getText)
+        console.log(constants.adjtransId);
+        cy.get(Adjustloc.Txn_id_search).type(constants.adjtransId);
     }
-    adjustmentsearchstatusselect()
-    {
-        cy.get(':nth-child(2) > .deselect-option').click()
-    }
-    adjustmentsearchclientaccnumber()
-    {
-        cy.get("#walletAccountNumber").type("7110045189")
-    }
-    adjustmentsearchremarks()
-    {
-        cy.get(':nth-child(3) > .odd > :nth-child(14)').should("contain","remarks")
-    }
-    adjustmentsearchinternalremarks()
-    {
-        cy.get('table#DataTables_Table_2>tbody>tr>td:nth-of-type(15)').should("contain","internalremarks")
-    }
+    // adjustmentsearchstatus()
+    // {
+    //     cy.get(':nth-child(2) > :nth-child(2) > .form-group > .col-sm-12 > .dama-select-box-forminput > .below > .single').click()
+    // }
+    // adjustmentsearchstatusselect()
+    // {
+    //     cy.get(':nth-child(2) > .deselect-option').click()
+    // }
+    // adjustmentsearchclientaccnumber()
+    // {
+    //     cy.get("#walletAccountNumber").type("7110045189")
+    // }
+    // adjustmentsearchremarks()
+    // {
+    //     cy.get(':nth-child(3) > .odd > :nth-child(14)').should("contain","remarks")
+    // }
+    // adjustmentsearchinternalremarks()
+    // {
+    //     cy.get('table#DataTables_Table_2>tbody>tr>td:nth-of-type(15)').should("contain","internalremarks")
+    // }
     adjustmentsearchsubmit()
     {
-        cy.get("button[type='submit']").click()
+        cy.get(Adjustloc.Search_submit).click()
     }
     adjustmentapprovebutton()
     {
-        cy.get(':nth-child(3) > .odd > td.ng-star-inserted > span.ng-star-inserted > :nth-child(1)').click()
+        
+        cy.get('.pull-right > .fa').click()
+        //cy.xpath("//table[@id='DataTables_Table_2']/tbody[1]/tr[1]/td[16]/span[1]/a[1]").click()
+        
+        cy.xpath(Adjustloc.Adj_approve).click()
+        cy.wait(1000)
     }
     adjustmentapprovebuttoniframe()
     {
-    cy.get(':nth-child(3) > #centralModalSuccess > .modal-dialog > .modal-content > .modal-footer > .btn-dark').click()
+    cy.get(Adjustloc.Approve_iframe).click()
  }
 }
 
