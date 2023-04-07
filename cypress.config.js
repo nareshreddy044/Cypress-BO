b2ea1942ec2a9b2aef889ed453198237321b9367
Naresh <nareshreddy044@gmail.com>
2023-04-07 09:33:44 +0530

    BO-0001

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,9 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",//for html report
+  reporter: "mochawesome",//for html report
   e2e: {
     setupNodeEvents(on, config) {
-
       require('cypress-mochawesome-reporter/plugin')(on);// for html report
       this.screenshotOnRunFailure=true;
       //experimentalSessionAndOrigin: true
