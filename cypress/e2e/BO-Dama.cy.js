59f2649b38f16fb17d7f36ff4b3fc47ecec0f8bb
Naresh <nareshreddy044@gmail.com>
2023-05-04 14:48:30 +0530

    DPS1-10025

--- cypress/e2e/BO-Dama.cy.js
+++ cypress/e2e/BO-Dama.cy.js
@@ -1,4 +1,3 @@
-import loginclass from "../Pages/codeblocks/login"
 import usernameassertion from "../Pages/codeblocks/usernameassertion"
 import transactiondashboard from "../Pages/codeblocks/transaction dasboard"
 import businessclientclass from "../Pages/codeblocks/businessclient"
@@ -8,19 +7,18 @@ import cashdepositclass from "../Pages/codeblocks/cashdeposit"
 import abc from "../fixtures/abc"
 describe('dama-BO', () => {
 abc.abcmethod()
-
 //it.only will execute that particular test case, else will execute all 
   it('BO-login', () => { const un=new usernameassertion();un.usernameassertion() })
   it('BO-transactions dasahboard search', () => { transactiondashboard.transactionsearch()})
   it('BO-transaction dashboard export', () => { transactiondashboard.transactiondashboardexportmethod() })
   it('BO-business client search view', () => { businessclientclass.businessclientsearch() })
-  it.only('BO-A2A transaction', () => { A2Atransferclass.A2Atransfermethod() })
-  it.only('BO-A2A transaction approve',() => { A2Atransferclass.A2Atransferapprovemethod() })
+  it('BO-A2A transaction', () => { A2Atransferclass.A2Atransfermethod() })
+  it('BO-A2A transaction approve',() => { A2Atransferclass.A2Atransferapprovemethod() })
   it('BO-A2A dashbaord export', () => { A2Atransferclass.A2Aexportmethod() })
-  it.only('BO-Credit Adjustment create', () => { adjustmentclass.adjustmentmethod() })
-  it.only('BO-Credit Adjustment approve', () => { adjustmentclass.adjustmentapprovemethod() })
-  it.only('BO-cash deposit create', () => { cashdepositclass.cashdepositmethod() })
-  it.only('BO-cashdepositapprove', () => { cashdepositclass.cashdepositmethodapprove()})
+  it('BO-Credit Adjustment create', () => { adjustmentclass.adjustmentmethod() })
+  it('BO-Credit Adjustment approve', () => { adjustmentclass.adjustmentapprovemethod() })
+  it('BO-cash deposit create', () => { cashdepositclass.cashdepositmethod() })
+  it('BO-cashdepositapprove', () => { cashdepositclass.cashdepositmethodapprove()})
   it('BO-cashdepositExport', () => { cashdepositclass.cashdepositmethodexport() })
   it('BO-cashdepositsearch', () => { cashdepositclass.cashdepositsearchviatransactiontype() })
   it('BO-cashdepositsearch', () => { cashdepositclass.cashdepositsearchviastatus() })
